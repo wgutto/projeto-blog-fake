209bf622087627c402fdb574459b8e72b14b89a7
Augusto Brito <121031260+wgutto@users.noreply.github.com>
2024-12-28 00:28:37 -0300

    project blog

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 // https://jsonplaceholder.typicode.com/posts
 
+
+// Adicionando os posts do blog
 async function readPosts() {
     let postArea = document.querySelector('.posts')
     postArea.innerHTML = 'Carregando posts...'
@@ -11,24 +13,22 @@ async function readPosts() {
         postArea.innerHTML = ''
 
         for(let i in json) {
-            let h1 = document.createElement('h1')
-            let p = document.createElement('p')
+            let title = document.createElement('h1')
+            let body = document.createElement('p')
             let hr = document.createElement('hr')
 
-            h1.append(`${json[i].title}`)
-            p.append(`${json[i].body}`)
+            title.append(`${json[i].title}`)
+            body.append(`${json[i].body}`)
             hr.append()
 
-            postArea.appendChild(h1)
-            postArea.appendChild(p)
+            postArea.appendChild(title)
+            postArea.appendChild(body)
             postArea.appendChild(hr)
-
         }
     } else {
-        postArea.innerHTML = 'Nenhum post encontrado.'
+        alert('Nenhum post encontrado.')
     }
-
-
+    
 /*
     if(json.length > 0) {
         postArea.innerHTML = ''
@@ -37,7 +37,42 @@ async function readPosts() {
         }
     } else {
         postArea.innerHTML = 'Nenhum post encontrado.'
-    } */
+    } 
+*/
 }
+
+// Requisição de postagem
+async function addNewPost(title, body) {
+    await fetch(
+        'https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                title,
+                body,
+                userId: 2
+            })
+        }
+    )
+    document.querySelector('#title').value = ''
+    document.querySelector('#body').value = ''
+
+    readPosts()
+}
+
+// Evento de clique no botão
+document.getElementById('button').addEventListener('click', () => {
+    let title = document.querySelector('#title')
+    let body = document.querySelector('#body')
+
+    if(title.value && body.value) {
+        addNewPost(title, body)
+    } else {
+        alert('Preencha os campos para continuar.')
+    }
+})
+
 
 readPosts()
